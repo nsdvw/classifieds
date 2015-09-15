265de90cdf2b1ae8f49b27f44ece5cd9e67688c1
inside <inside@inside.com>
2015-09-16 01:35:27 +0300

    ad/new - page with ajax widget for category choice

---
+++ protected/controllers/AdController.php
@@ -0,0 +1,210 @@
+<?php
+
+class AdController extends Controller
+{
+	/**
+	 * @return array action filters
+	 */
+	public function filters()
+	{
+		return array(
+			'accessControl', // perform access control for CRUD operations
+			'postOnly + delete', // we only allow deletion via POST request
+		);
+	}
+
+	/**
+	 * Specifies the access control rules.
+	 * This method is used by the 'accessControl' filter.
+	 * @return array access control rules
+	 */
+	public function accessRules()
+	{
+		return array(
+			array('allow',  // allow all users to perform 'index' and 'view' actions
+				'actions'=>array('index','view','new','getcategories','create'),
+				'users'=>array('*'),
+			),
+			array('allow', // allow authenticated user to perform 'create' and 'update' actions
+				'actions'=>array('update'),
+				'users'=>array('@'),
+			),
+			array('allow', // allow admin user to perform 'admin' and 'delete' actions
+				'actions'=>array('admin','delete'),
+				'users'=>array('admin'),
+			),
+			array('deny',  // deny all users
+				'users'=>array('*'),
+			),
+		);
+	}
+
+	/**
+	 * Displays a particular model.
+	 * @param integer $id the ID of the model to be displayed
+	 */
+	public function actionView($id)
+	{
+		$this->render('view',array(
+			'model'=>$this->loadModel($id),
+		));
+	}
+
+	/**
+	 * Displays the page with category selection. When user choose category, redirect
+	 * to the 'create' page.
+	 */
+	public function actionNew()
+	{
+		$criteria = new CDbCriteria;
+		$criteria->condition = 'level=:level';
+		$criteria->params = array('level'=>1);
+		//$dp = new CActiveDataProvider('Category', array('criteria'=>$criteria));
+		$models = Category::model()->findAll($criteria);
+		$this->render('new', array('models'=>$models));
+	}
+
+	public function actionGetcategories()
+	{
+		if (!isset($_POST['id'])) {
+			echo json_encode(array());
+			Yii::app()->end();
+		}
+		//echo 'id='.$_POST['id'].',level='.$_POST['level'];
+		$id = intval($_POST['id']);
+		$parent_cat = Category::model()->findByPk($id);
+		//echo json_encode(array($parent_cat->title));
+		$criteria = new CDbCriteria;
+		$criteria->condition = 'root=:root AND lft>:lft AND rgt<:rgt AND level=:level';
+		$criteria->params = array(
+			':root'=>$parent_cat->root,
+			':lft'=>$parent_cat->lft,
+			':rgt'=>$parent_cat->rgt,
+			':level'=>++$parent_cat->level,
+		);
+		$children = Category::model()->findAll($criteria);
+		if (!$children) {
+			echo json_encode(array());
+			Yii::app()->end();
+		}
+		foreach ($children as $child) {
+			$res[$child->id] = $child->title;
+		}
+		echo json_encode($res);
+	}
+
+	/**
+	 * Creates a new model.
+	 * If creation is successful, the browser will be redirected to the 'view' page.
+	 */
+	public function actionCreate($id)
+	{
+		$model=new Ad;
+		echo 'Hello! The category id was '.$id;
+		// Uncomment the following line if AJAX validation is needed
+		// $this->performAjaxValidation($model);
+
+		/*if(isset($_POST['Ad']))
+		{
+			$model->attributes=$_POST['Ad'];
+			if($model->save())
+				$this->redirect(array('view','id'=>$model->id));
+		}
+
+		$this->render('create',array(
+			'model'=>$model,
+		));*/
+	}
+
+	/**
+	 * Updates a particular model.
+	 * If update is successful, the browser will be redirected to the 'view' page.
+	 * @param integer $id the ID of the model to be updated
+	 */
+	public function actionUpdate($id)
+	{
+		$model=$this->loadModel($id);
+
+		// Uncomment the following line if AJAX validation is needed
+		// $this->performAjaxValidation($model);
+
+		if(isset($_POST['Ad']))
+		{
+			$model->attributes=$_POST['Ad'];
+			if($model->save())
+				$this->redirect(array('view','id'=>$model->id));
+		}
+
+		$this->render('update',array(
+			'model'=>$model,
+		));
+	}
+
+	/**
+	 * Deletes a particular model.
+	 * If deletion is successful, the browser will be redirected to the 'admin' page.
+	 * @param integer $id the ID of the model to be deleted
+	 */
+	public function actionDelete($id)
+	{
+		$this->loadModel($id)->delete();
+
+		// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
+		if(!isset($_GET['ajax']))
+			$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+	}
+
+	/**
+	 * Lists all models.
+	 */
+	public function actionIndex()
+	{
+		$dataProvider=new CActiveDataProvider('Ad');
+		$this->render('index',array(
+			'dataProvider'=>$dataProvider,
+		));
+	}
+
+	/**
+	 * Manages all models.
+	 */
+	public function actionAdmin()
+	{
+		$model=new Ad('search');
+		$model->unsetAttributes();  // clear any default values
+		if(isset($_GET['Ad']))
+			$model->attributes=$_GET['Ad'];
+
+		$this->render('admin',array(
+			'model'=>$model,
+		));
+	}
+
+	/**
+	 * Returns the data model based on the primary key given in the GET variable.
+	 * If the data model is not found, an HTTP exception will be raised.
+	 * @param integer $id the ID of the model to be loaded
+	 * @return Ad the loaded model
+	 * @throws CHttpException
+	 */
+	public function loadModel($id)
+	{
+		$model=Ad::model()->findByPk($id);
+		if($model===null)
+			throw new CHttpException(404,'The requested page does not exist.');
+		return $model;
+	}
+
+	/**
+	 * Performs the AJAX validation.
+	 * @param Ad $model the model to be validated
+	 */
+	protected function performAjaxValidation($model)
+	{
+		if(isset($_POST['ajax']) && $_POST['ajax']==='ad-form')
+		{
+			echo CActiveForm::validate($model);
+			Yii::app()->end();
+		}
+	}
+}
