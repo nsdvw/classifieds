b6e719ee729f089974c9cfbdc45bcd32874814ec
inside <inside@inside.com>
2015-09-17 21:59:29 +0300

    Registration and login

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -15,19 +15,22 @@ class UserIdentity extends CUserIdentity
 	 * against some persistent user identity storage (e.g. database).
 	 * @return boolean whether authentication succeeds.
 	 */
+
+	private $_id;
+	
 	public function authenticate()
 	{
-		$users=array(
-			// username => password
-			'demo'=>'demo',
-			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		elseif($users[$this->username]!==$this->password)
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		else
-			$this->errorCode=self::ERROR_NONE;
-		return !$this->errorCode;
+		$record = User::model()->findByAttributes(array('email'=>$this->username));
+        if($record===null)
+            $this->errorCode=self::ERROR_USERNAME_INVALID;
+        else if(!CPasswordHelper::verifyPassword($this->password, $record->password))
+            $this->errorCode=self::ERROR_PASSWORD_INVALID;
+        else
+        {
+            $this->_id=$record->id;
+            $this->setState('name', $record->name);
+            $this->errorCode=self::ERROR_NONE;
+        }
+        return !$this->errorCode;
 	}
 }
--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -1,6 +1,6 @@
 <?php
 
-class AdController extends Controller
+class UserController extends Controller
 {
 	/**
 	 * @return array action filters
@@ -22,15 +22,15 @@ class AdController extends Controller
 	{
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
+				'actions'=>array('register'),
 				'users'=>array('*'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('update','new','getcategories','create'),
+				'actions'=>array('update','view'),
 				'users'=>array('@'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
+				'actions'=>array('admin','delete', 'index'),
 				'users'=>array('admin'),
 			),
 			array('deny',  // deny all users
@@ -51,73 +51,24 @@ class AdController extends Controller
 	}
 
 	/**
-	 * Displays the page with category selection. When user choose category, redirect
-	 * to the 'create' page.
-	 */
-	public function actionNew()
-	{
-		$criteria = new CDbCriteria;
-		$criteria->condition = 'level=:level';
-		$criteria->params = array('level'=>1);
-		//$dp = new CActiveDataProvider('Category', array('criteria'=>$criteria));
-		$models = Category::model()->findAll($criteria);
-		$this->render('new', array('models'=>$models));
-	}
-
-	/*
-	 * Responds to ajax request from ad/new page
-	 */
-	public function actionGetcategories()
-	{
-		if (!isset($_POST['id'])) {
-			echo json_encode(array());
-			Yii::app()->end();
-		}
-		$id = intval($_POST['id']);
-		$parent_cat = Category::model()->findByPk($id);
-		$children = $parent_cat->children()->findAll();
-		if (!$children) {
-			echo json_encode(array());
-			Yii::app()->end();
-		}
-		foreach ($children as $child) {
-			$res[$child->id] = $child->title;
-		}
-		echo json_encode($res);
-	}
-
-	/**
 	 * Creates a new model.
 	 * If creation is successful, the browser will be redirected to the 'view' page.
 	 */
-	public function actionCreate($id)
+	public function actionRegister()
 	{
-		$model = new Ad;
-		$category = Category::model()->findByPk($id);
-		$children = $category->children()->findAll();
-		$children = CHtml::listData($children, 'id', 'title');
-		$hasChildren = ($children) ? true : false; 
-		$model->attachEavSet($category->set_id);
-		$model->category_id = $id;
+		$model = new User;
+		$this->performAjaxValidation($model);
 
-		$lists = array('category' => $children);
-		foreach ($model->eavAttributes as $key => $value) {
-			$attribute = EavAttribute::model()->findByAttributes(array('name'=>$key));
-			$variants =	AttrVariant::model()->findAllByAttributes(
-				array('attr_id'=>$attribute->id)
-			);
-			if (!$variants) continue;
-			$lists[$key] = CHtml::listData($variants, 'title', 'title');
+		if(isset($_POST['User']))
+		{
+			$model->attributes = $_POST['User'];
+			$model->password = CPasswordHelper::hashPassword($model->password);
+			if($model->save())
+				$this->redirect(array('site/login'));
 		}
 
-		if (isset($_POST['Ad'])) {
-			$model->attributes=$_POST['Ad'];
-			if($model->saveWithEavAttributes())
-				$this->redirect(array('view','id'=>$model->id));
-		}
-		
-		$this->render('create',array(
-			'model'=>$model, 'hasChildren'=>$hasChildren, 'lists'=>$lists
+		$this->render('register',array(
+			'model'=>$model,
 		));
 	}
 
@@ -133,9 +84,9 @@ class AdController extends Controller
 		// Uncomment the following line if AJAX validation is needed
 		// $this->performAjaxValidation($model);
 
-		if(isset($_POST['Ad']))
+		if(isset($_POST['User']))
 		{
-			$model->attributes=$_POST['Ad'];
+			$model->attributes=$_POST['User'];
 			if($model->save())
 				$this->redirect(array('view','id'=>$model->id));
 		}
@@ -164,7 +115,7 @@ class AdController extends Controller
 	 */
 	public function actionIndex()
 	{
-		$dataProvider=new CActiveDataProvider('Ad');
+		$dataProvider=new CActiveDataProvider('User');
 		$this->render('index',array(
 			'dataProvider'=>$dataProvider,
 		));
@@ -175,10 +126,10 @@ class AdController extends Controller
 	 */
 	public function actionAdmin()
 	{
-		$model=new Ad('search');
+		$model=new User('search');
 		$model->unsetAttributes();  // clear any default values
-		if(isset($_GET['Ad']))
-			$model->attributes=$_GET['Ad'];
+		if(isset($_GET['User']))
+			$model->attributes=$_GET['User'];
 
 		$this->render('admin',array(
 			'model'=>$model,
@@ -189,12 +140,12 @@ class AdController extends Controller
 	 * Returns the data model based on the primary key given in the GET variable.
 	 * If the data model is not found, an HTTP exception will be raised.
 	 * @param integer $id the ID of the model to be loaded
-	 * @return Ad the loaded model
+	 * @return User the loaded model
 	 * @throws CHttpException
 	 */
 	public function loadModel($id)
 	{
-		$model=Ad::model()->findByPk($id);
+		$model=User::model()->findByPk($id);
 		if($model===null)
 			throw new CHttpException(404,'The requested page does not exist.');
 		return $model;
@@ -202,11 +153,11 @@ class AdController extends Controller
 
 	/**
 	 * Performs the AJAX validation.
-	 * @param Ad $model the model to be validated
+	 * @param User $model the model to be validated
 	 */
 	protected function performAjaxValidation($model)
 	{
-		if(isset($_POST['ajax']) && $_POST['ajax']==='ad-form')
+		if(isset($_POST['ajax']) && $_POST['ajax']==='user-form')
 		{
 			echo CActiveForm::validate($model);
 			Yii::app()->end();
