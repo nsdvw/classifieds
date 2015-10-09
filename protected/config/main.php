d388ad95f36d18a843adae0549a3ccefbc7fa355
inside <inside@inside.com>
2015-10-09 20:14:50 +0300

    Code for fake eav-attrs generation

--- protected/config/main.php
+++ protected/config/main.php
@@ -82,9 +82,9 @@ return array(
 				),
 				// uncomment the following to show log messages on web pages
 				
-				array(
+				/*array(
 					'class'=>'CWebLogRoute',
-				),
+				),*/
 				
 			),
 		),
--- protected/controllers/FakerController.php
+++ protected/controllers/FakerController.php
@@ -8,18 +8,20 @@ class FakerController extends Controller
 {
     public function actionIndex()
     {
-        set_time_limit(3600 * 2);
+        set_time_limit(3600 * 3);
         $faker = Faker\Factory::create('ru_RU');
 
         echo date('H:i:s') . ' Start!<br>';
-        echo date('H:i:s') . ' Begin to create users.<br>';
+        echo date('H:i:s') . ' Begin creating users.<br>';
         /*for ($i=0; $i < 100; $i++) {
-            // more than 1000 is not safe; you may increase max_allowed_packet
             $this->createLegion($faker, 1000);
         }*/
         echo date('H:i:s') . ' End creating users.<br>';
 
         $sets = EavSet::model()->with('eavAttribute')->findAll();
+        foreach ($sets as $s) {
+            $setsAssoc[$s->id] = $s;
+        }
 
         $sql = "SELECT city_id FROM city WHERE country_id=3159";
         $cities = Yii::app()->db->createCommand($sql)->queryColumn();
@@ -28,15 +30,30 @@ class FakerController extends Controller
         $users = Yii::app()->db->createCommand($sql)->queryColumn();
 
         $categories = $this->getCategories();
-        echo date('H:i:s') . ' Begin to create ads.<br>';
+        echo date('H:i:s') . ' Begin creating ads.<br>';
         /*foreach ($categories as $c) {
             if ($c['lft'] + 1 != $c['rgt']) continue; // search leaves
-            for ($i=0; $i < 33; $i++) {
-                // more than 100 at once is not safe; you may increase max_allowed_packet
-                $this->createAds($faker, $c, $cities, $users, 100);
+            for ($i=0; $i < 1; $i++) {
+                $this->createAds($faker, $c, $cities, $users, 50);
             }
+            // break;
         }*/
         echo date('H:i:s') . ' End creating ads.<br>';
+
+        echo date('H:i:s') . ' Begin attaching eav-attributes to ads.<br>';
+        $sql = "SELECT count(*) FROM ad";
+        $totalCount = intval(Yii::app()->db->createCommand($sql)->queryScalar());
+        $perQuery = 10000;
+        $offset = 0;
+        while ($offset < /*$totalCount*/ 1) {
+            $transaction = Yii::app()->db->beginTransaction();
+            $sql = "SELECT id, eav_set_id FROM ad LIMIT {$offset}, {$perQuery}";
+            $ads = Yii::app()->db->createCommand($sql)->queryAll();
+            $this->attachEavAttributes($ads, $setsAssoc);
+            $offset += $perQuery;
+            $transaction->commit();
+        }
+        echo date('H:i:s') . ' End attaching eav-attributes to ads.<br>';
 
         echo date('H:i:s') . ' Finish!<br>';
     }
@@ -48,7 +65,7 @@ class FakerController extends Controller
         for ($i=0; $i < $num; $i++) {
             // to avoid duplicates on unique field email use random prefix
             $user['email'] = $alphabet[rand(0, 25)] . rand(1000, 9999) . $faker->freeEmail;
-            // problem with escape symbols when using ::password
+            // problem with escape symbols when using faker password
             $user['password'] = $faker->word . $faker->word; 
             $user['name'] = $faker->name;
             $user['phone'] = $faker->phoneNumber;
@@ -72,14 +89,18 @@ class FakerController extends Controller
         array $category,
         array $cities,
         array $authors,
-        $num = 100)
+        $num = 1000)
     {
         $sql = "INSERT INTO ad
         (title, description, eav_set_id, category_id, city_id, author_id) VALUES ";
         for ($i = 0; $i < $num; $i++) {
             $ad = array();
             $ad['title'] = $faker->sentence;
-            $ad['description'] = $faker->realText;
+            /*
+            ошибка при использовании realText:
+            CDbCommand не удалось исполнить SQL-запрос: SQLSTATE[HY093]: Invalid parameter number: no parameters were bound.
+            */
+            $ad['description'] = $faker->text;
             $ad['eav_set_id'] = $category['set_id'];
             $ad['category_id'] = $category['id'];
             $ad['city_id'] = $cities[array_rand($cities)];
@@ -91,5 +112,32 @@ class FakerController extends Controller
             }
         }
         Yii::app()->db->createCommand($sql)->execute();
+    }
+
+    private function attachEavAttributes(array $ads, array $sets)
+    {
+        foreach ($ads as $ad) {
+            $set = $sets[$ad['eav_set_id']];
+            foreach ($set->eavAttribute as $a) {
+                $rawData = unserialize($a->data);
+                if ($a->data_type == 'IntDataType') {
+                    $tbl = 'eav_attribute_int';
+                    $min = (isset($rawData['rules']['numerical']['min']))
+                            ? intval($rawData['rules']['numerical']['min'])
+                            : 0;
+                    $max = (isset($rawData['rules']['numerical']['max']))
+                            ? intval($rawData['rules']['numerical']['max'])
+                            : 100000000;
+                    $value = rand($min, intval($max/100));
+                } elseif ($a->data_type == 'VarcharDataType') {
+                    $tbl = 'eav_attribute_varchar';
+                    $value = array_rand($rawData['values']);
+                }
+                $sql = 
+                    "INSERT INTO {$tbl} (eav_attribute_id, entity_id, entity, value)
+                    VALUES ('{$a->id}', '{$ad['id']}', 'ad', '$value')";
+                Yii::app()->db->createCommand($sql)->execute();
+            }
+        }
     }
 }
