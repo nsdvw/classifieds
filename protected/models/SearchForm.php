5ace5066e323941ae55349a82f96045f032ac18a
inside <inside@inside.com>
2016-01-21 18:06:15 +0200

    Drop getRegions method, use Regions::getRegionList instead

--- protected/models/SearchForm.php
+++ protected/models/SearchForm.php
@@ -5,12 +5,6 @@ class SearchForm extends CFormModel
     public $region_id;
     public $word;
 
-    public function getRegions($country_id = 3159) // default to Russia
-    {
-        $regions = Region::model()->findAllByAttributes(array('country_id'=>$country_id));
-        return CHtml::listData($regions, 'region_id', 'name');
-    }
-
     public function getCities($region_id)
     {
         $cities = City::model()->findAll('region_id=:region_id', array(':region_id'=>$region_id));
