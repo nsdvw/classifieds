f401141de2398bc7095935d500049ee5181fe711
inside <inside@inside.com>
2016-01-21 18:05:29 +0200

    Use PreviewGenarator package instead of disposable code

--- protected/models/Photo.php
+++ protected/models/Photo.php
@@ -1,5 +1,8 @@
 <?php
 
+use PreviewGenerator\Adapter\WideImageAdapter;
+use PreviewGenerator\PreviewGenerator;
+
 /**
  * This is the model class for table "photo".
  *
@@ -14,12 +17,6 @@
  */
 class Photo extends CActiveRecord
 {
-	const SM_THUMB_SIZE = 170;
-	const SM_THUMB_PREFIX = 'small';
-	const BG_THUMB_SIZE = 800;
-	const BG_THUMB_PREFIX = 'big';
-	const TN_THUMB_SIZE = 50;
-	const TN_THUMB_PREFIX = 'tiny';
 	public $image;
 	/**
 	 * @return string the associated database table name
@@ -34,8 +31,6 @@ class Photo extends CActiveRecord
 	 */
 	public function rules()
 	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
 		return array(
 			array('image', 'file', 'types'=>'jpg, gif, png'),
 			// The following rule is used by search().
@@ -49,8 +44,6 @@ class Photo extends CActiveRecord
 	 */
 	public function relations()
 	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
 		return array(
 			'ad' => array(self::BELONGS_TO, 'Ad', 'ad_id'),
 		);
@@ -114,11 +107,16 @@ class Photo extends CActiveRecord
 		if ($this->isNewRecord) {
 			$name = "{$this->id}_{$this->name}";
 			$path = Yii::getPathOfAlias('webroot').'/upload/'.$name;
+            $logo = Yii::getPathOfAlias('webroot').'/images/logo.png';
+            $thumbDir = Yii::getPathOfAlias('webroot').'/images/thumb/';
+            $thumbPath = $thumbDir . 'small_' . $name;
 			$this->image->saveAs($path);
-			$this->putWatermark($path);
-			$this->createThumb($name, 170/120);
-			$this->createThumb($name, 4/3, self::BG_THUMB_SIZE, self::BG_THUMB_PREFIX);
-			$this->createThumb($name, 4/3, self::TN_THUMB_SIZE, self::TN_THUMB_PREFIX);
+            $adapter = new WideImageAdapter;
+            $previewGenerator = new PreviewGenerator($adapter);
+            $previewGenerator->load($path);
+            $previewGenerator->putWatermark($logo)
+                ->create(160, 120, 245, 245, 245)
+                ->save($thumbPath);
 		}
 		parent::afterSave();
 	}
@@ -128,48 +126,6 @@ class Photo extends CActiveRecord
 		Yii::setPathOfAlias('WideImage',Yii::getPathOfAlias(
 			'application.vendor.smottt.wideimage.lib.WideImage'
 		));
-	}
-
-	protected function putWatermark(
-		$pathToPhoto,
-		$coef = 4)
-	{
-		$pathToLogo = Yii::getPathOfAlias('webroot').'/images/logo.png';
-		$img = WideImage\WideImage::loadFromFile($pathToPhoto);
-		$watermark = WideImage\WideImage::loadFromFile($pathToLogo);
-		if ($img->getWidth() > $img->getHeight()) {
-		    $ratio = $img->getHeight() / $watermark->getHeight();
-		    $multiplier = intval(($ratio / $coef) * 100) . '%';
-		    $watermark = $watermark->resize(null, $multiplier);
-		    $margin = intval($img->getHeight() / 20);
-		    $new_img = $img->merge($watermark, "right - $margin", "bottom - $margin");
-		} else {
-		    $ratio = $img->getWidth() / $watermark->getWidth();
-		    $multiplier = intval(($ratio / $coef) * 100) . '%';
-		    $watermark = $watermark->resize($multiplier, null);
-		    $margin = intval($img->getHeight() / 20);
-		    $new_img = $img->merge($watermark, "right - $margin", "bottom - $margin");
-		}
-		$new_img->saveToFile($pathToPhoto);
-	}
-
-	protected function createThumb(
-		$name,
-		$ratio = 1.33,
-		$size = self::SM_THUMB_SIZE,
-		$label = self::SM_THUMB_PREFIX,
-		$uploadDir = null,
-		$thumbDir = null)
-	{
-		if (!$uploadDir) $uploadDir = Yii::getPathOfAlias('webroot').'/upload/';
-		if (!$thumbDir) $thumbDir = Yii::getPathOfAlias('webroot').'/images/thumb/';
-		$img = WideImage\WideImage::loadFromFile($uploadDir . $name);
-		if ($img->getWidth() > $ratio * $img->getHeight()) {
-		    $new_img = $img->resizeDown(intval($size), null);
-		} else {
-		    $new_img = $img->resizeDown(null, intval($size/$ratio));
-		}
-		$new_img->saveToFile($thumbDir . $label . '_' . $name);
 	}
 
 	public static function validateMultiple(array $images, $model_id)
